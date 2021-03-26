d623219d542cf5a97b879dfc1dc9a4a25bb0cf93
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-03-25 22:27:37 -0300

    feature: create the first variable

--- src/aula/Main.java
+++ src/aula/Main.java
@@ -2,6 +2,8 @@ package aula;
 
 public class Main {
 	public static void main(String[] args) {
+		int idade = 24;
+		System.out.println("Minha idade é: "+ idade);
 		System.out.println("Hello World");
 	}
 }
