719c9d8cb4ef940f25cf451088f8efca6719829e
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-03-26 00:34:49 -0300

    feature: type of variables

--- src/aula/Main.java
+++ src/aula/Main.java
@@ -2,8 +2,23 @@ package aula;
 
 public class Main {
 	public static void main(String[] args) {
-		int idade = 24;
-		System.out.println("Minha idade é: "+ idade);
-		System.out.println("Hello World");
+		/*
+			int idade = 24;
+			System.out.println("Minha idade é: "+ idade);
+			System.out.println("Hello World");
+		*/
+		
+		// Tipos de váriaveis
+		int numero = 24;
+		String nome = "Yan";
+		boolean depende = true;
+		char umCaractere = 'a';
+		double pontoFlutuante = 2.3;
+		
+		System.out.println(numero);
+		System.out.println(nome);
+		System.out.println(depende);
+		System.out.println(umCaractere);
+		System.out.println(pontoFlutuante);
 	}
 }
