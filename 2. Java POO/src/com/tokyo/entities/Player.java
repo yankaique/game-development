90fe3624e758a1f6baae51f084f8f35af2afbbf2
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-02 11:37:56 -0300

    feature: create first packages

---
+++ 2. Java POO/src/com/tokyo/entities/Player.java
@@ -0,0 +1,5 @@
+package com.tokyo.entities;
+
+public class Player {
+	
+}
---
+++ 2. Java POO/src/com/tokyo/graphics/Main.java
@@ -0,0 +1,9 @@
+package com.tokyo.graphics;
+
+import com.tokyo.entities.Player;
+
+public class Main {
+	public static void main(String[] args) {
+		Player player = new Player();
+	}
+}
