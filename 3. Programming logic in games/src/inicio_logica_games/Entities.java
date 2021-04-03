b6dfcb11bed89e5decea24b27729f239ea7fdfe7
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-03 09:16:23 -0300

    feature: array list in thread

---
+++ 3. Programming logic in games/src/inicio_logica_games/Entities.java
@@ -0,0 +1,7 @@
+package inicio_logica_games;
+
+public class Entities {
+	public Entities() {
+		
+	}
+}
--- 3. Programming logic in games/src/inicio_logica_games/Game.java
+++ 3. Programming logic in games/src/inicio_logica_games/Game.java
@@ -1,9 +1,22 @@
 package inicio_logica_games;
 
+import java.util.ArrayList;
+
 public class Game implements Runnable{
 	
 	private boolean isRunning;
 	private Thread thread;
+	private ArrayList<Entities> entidades = new ArrayList<>();
+	
+	public Game() {
+		entidades.add(new Entities());
+		entidades.add(new Entities());
+		entidades.add(new Entities());
+		entidades.add(new Entities());
+		for(int i =0; i < entidades.size(); i++) {
+			System.out.println(i);
+		}
+	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
@@ -18,12 +31,12 @@ public class Game implements Runnable{
 	
 	public void update() {
 		// Atualizar o jogo
-		System.out.println("Update !!!");
+//		System.out.println("Update !!!");
 	}
 	
 	public void render() {
 		// Renderizar o jogo
-		System.out.println("Render !!!");
+//		System.out.println("Render !!!");
 	}
 	
 	@Override
