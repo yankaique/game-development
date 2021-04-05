1cb67f25ceed1e815edc6615144172e842175c7a
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-05 07:03:30 -0300

    feature: profissional looping

--- 4. Graphics/src/graficos/Game.java
+++ 4. Graphics/src/graficos/Game.java
@@ -8,12 +8,17 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable{
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
 	private final int WIDTH = 160;
 	private final int HEIGHT = 120;
 	private final int SCALE = 3;
 	
 	public Game() {
-		this.setPreferredSize((new Dimension(WIDTH*SCALE, HEIGHT*SCALE)));
+		setPreferredSize((new Dimension(WIDTH*SCALE, HEIGHT*SCALE)));
+	}
+	
+	public void initFrame() {
 		frame = new JFrame("Meu Game");
 		frame.add(this);
 		frame.setResizable(false);
@@ -23,12 +28,64 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.start();
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
 	}
 
 	public void run() {
+		// Tempo atual preciso
+		long lastTime = System.nanoTime();
 		
+		// FPS
+		double amountOfTicks = 60.0;
+		
+		// Calculo para pegar momento certo
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		
+		// debug
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		
+		while(isRunning) {
+			
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			if(delta >= 1) {
+				update();
+				render();
+				frames ++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: "+frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
 		
 	}
 	
